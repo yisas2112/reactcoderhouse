b1f035263896eb0d15f4c04b244f7e30aad151f6
jesus rodriguez <rodriguezejesu1992@gmail.com>
2020-11-13 18:19:40 -0300

    Corrigiendo Desafío clase 6 (2)

--- src/components/Productos/item.js
+++ src/components/Productos/item.js
@@ -1,26 +1,26 @@
 import React from 'react';  
 
 const Item = ({props}) =>{    
-    const number = props.id;
+    const number = props.products.id;
     return (
         <>         
              <div className="col-3" key={number}>
                         <div className="card card-cascade card-ecommerce m-3">
                             {/* Card Image */}
                             <div className="view view-cascade overlay">                                
-                                <img className="card-img-top" src={props.img}></img>
+                                <img className="card-img-top" src={props.products.img}></img>
                                 <a>
                                     <div className="mask rgba-white-slight"></div>
                                 </a>
                             </div>
                             {/* Card Content */}                            
                             <div className="card-body card-body-cascade text-center">   
-                                <h5>{props.product.Model} - {props.product.year}</h5>
+                                <h5>{props.products.Model} - {props.products.year}</h5>
                                 <button type="button" className="btn btn-primary">Comprar</button>
                             </div>
                             {/* Card Footer */}                            
                             <div className="card-footer m-auto w-100">
-                                <span>{props.product.Price}</span>                                                                
+                                <span>{props.products.Price}</span>                                                                
                             </div>                            
                         </div>
                     </div>
