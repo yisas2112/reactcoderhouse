fb8bb474c079b38ca48de28d72f57c35518f7cba
jesus rodriguez <rodriguezejesu1992@gmail.com>
2020-11-13 18:16:36 -0300

    Corrigiendo Desafío clase 6

--- src/components/Productos/ItemList.js
+++ src/components/Productos/ItemList.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';  
 import Item from './item';
+import Data from './productos.json';
 
 const ItemList = () =>{
-    return (
-        <>
-        <div className="text-center w-100">
-        <Item/>
-        </div>
-        
-        </>
-    )
+    const[product, setproduct] = useState();
+
+    useEffect(()=>{
+        getProduct();        
+    },[]);
+
+
+    const getProduct =() =>{
+        const server = new Promise(resolve =>{
+
+            setTimeout(()=>{resolve(Data)},2000);
+        })
+        server.then(response=>{
+            setproduct(response);
+        });
+    }
+
+
+    const ReturnProducts = () =>{
+        if(!product){
+            return <p className="m-3">...Cargando Productos</p>
+        }
+        product.map(products =><Item products={products}/>)
+    
+    }
+
+    return <>
+    <div>
+        <h2 className="h2">Productos</h2>
+        <div className="row w-100">
+            
+        <ReturnProducts/>
+        </div> 
+    </div>    
+    </>
 }
 
 
--- src/components/Productos/item.js
+++ src/components/Productos/item.js
@@ -1,70 +1,31 @@
-import React, {useState, useEffect} from 'react';  
-import Data from './productos.json';
+import React from 'react';  
 
-
-const Item = () =>{
-    const[product, setproduct] = useState();
-
-    useEffect(()=>{
-        getProduct();        
-    },[]);
-
-
-    const getProduct =() =>{
-        const server = new Promise(resolve =>{
-
-            setTimeout(()=>{resolve(Data)},2000);
-        })
-        server.then(response=>{
-            setproduct(response);
-        });
-    }
-
-
-    const ReturnProducts = () =>{
-        if(!product){
-            return <p className="m-3">...Cargando Productos</p>
-        }
-        const list = product.map((elemento, index)=>{
-            while(elemento.id <= 20){
-            return <div className="col-3" key={index}>
+const Item = ({props}) =>{    
+    const number = props.id;
+    return (
+        <>         
+             <div className="col-3" key={number}>
                         <div className="card card-cascade card-ecommerce m-3">
                             {/* Card Image */}
                             <div className="view view-cascade overlay">                                
-                                <img className="card-img-top" src={elemento.img}></img>
+                                <img className="card-img-top" src={props.img}></img>
                                 <a>
                                     <div className="mask rgba-white-slight"></div>
                                 </a>
                             </div>
                             {/* Card Content */}                            
                             <div className="card-body card-body-cascade text-center">   
-                                <h5>{elemento.Model} - {elemento.year}</h5>
+                                <h5>{props.product.Model} - {props.product.year}</h5>
                                 <button type="button" className="btn btn-primary">Comprar</button>
                             </div>
-                            {/* Card Footer */}
-                            
+                            {/* Card Footer */}                            
                             <div className="card-footer m-auto w-100">
-                                <span>{elemento.Price}</span>                                                                
+                                <span>{props.product.Price}</span>                                                                
                             </div>                            
                         </div>
                     </div>
-            }    })
-        
-        return list;
-    
-    }
-
-    return <>
-    <div>
-        <h2 className="h2">Productos</h2>
-        <div className="row w-100">
-            
-        <ReturnProducts/>
-        </div> 
-    </div>    
-    </>
-
-
+        </>
+    )
 }
 
 
